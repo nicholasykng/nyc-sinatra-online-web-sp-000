069b225ce86f6dff1afa279765e5eb1ca2bee6b1
Nicholas Ng <nicholasykng@gmail.com>
2020-01-11 22:22:33 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -3,9 +3,15 @@ class LandmarksController < ApplicationController
     @landmarks = Landmark.all
     erb :"/landmarks/index"
   end
-  
+
+  get "/landmarks/new" do
+    erb :"/landmarks/new"
+  end
+
   get "/landmarks/:id" do
     @landmark = Landmark.find(params[:id])
     erb :"/landmarks/show"
   end
+
+
 end
